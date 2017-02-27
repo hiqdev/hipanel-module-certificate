b58a5f5355ee0a96e241f4ae9e2c3b7d908f569c
tofid <andreyklochok@gmail.com>
2017-02-27 19:54:35 +0200

    Added brands

--- src/models/CertificateType.php
+++ src/models/CertificateType.php
@@ -2,6 +2,7 @@
 
 namespace hipanel\modules\certificate\models;
 
+use hipanel\helpers\StringHelper;
 use Yii;
 use yii\base\Model;
 
@@ -22,25 +23,77 @@ class CertificateType extends Model
         return [
             'dv' => [
                 'label' => Yii::t('hipanel:certificate', 'Domain Validation'),
-                'text' => Yii::t('hipanel:certificate', ''),
+                'text' => Yii::t('hipanel:certificate', 'Get no-frills, industry standard encryption for a cheap price with our DV SSL Certificates'),
             ],
-            'mdc' => [
+            'sun' => [
                 'label' => Yii::t('hipanel:certificate', 'Multi-Domain Certs'),
-                'text' => Yii::t('hipanel:certificate', ''),
+                'text' => Yii::t('hipanel:certificate', 'Secure multiple domains on a single certificate for a cheap price with Multi-Domain SSL'),
             ],
             'ov' => [
                 'label' => Yii::t('hipanel:certificate', 'Organization Validation'),
-                'text' => Yii::t('hipanel:certificate', ''),
+                'text' => Yii::t('hipanel:certificate', 'Get light business authentication at an extremely cheap price with our OV SSL Certificates'),
             ],
             'wc' => [
                 'label' => Yii::t('hipanel:certificate', 'Wildcard Certificates'),
-                'text' => Yii::t('hipanel:certificate', ''),
+                'text' => Yii::t('hipanel:certificate', 'Secure unlimited Sub-Domains for one cheap price with these Wildcard SSL Certificates'),
             ],
-            'wv' => [
+            'ev' => [
                 'label' => Yii::t('hipanel:certificate', 'Extended Validation'),
-                'text' => Yii::t('hipanel:certificate', ''),
+                'text' => Yii::t('hipanel:certificate', 'Inspire maximum trust at an unbeatable price with an Extended Validation SSL'),
             ],
         ];
+    }
+
+    public static function brands()
+    {
+        return [
+            'symantec' => [
+                'label' => Yii::t('hipanel:certificate', 'Symantec SSL Certificates'),
+                'img' => 'https://cdn.ukrnames.com/theme/images/ssl-vendors/symantec_vendor.png',
+            ],
+            'ukrnames' => [
+                'label' => Yii::t('hipanel:certificate', 'Ukrnames SSL Certificates')
+            ],
+            'ggssl' => [
+                'label' => Yii::t('hipanel:certificate', 'GoGetSSL SSL Certificates'),
+            ],
+            'thawte' => [
+                'label' => Yii::t('hipanel:certificate', 'Thawte SSL Certificates'),
+                'img' => 'https://cdn.ukrnames.com/theme/images/ssl-vendors/thawte_vendor.png',
+            ],
+            'geotrust' => [
+                'label' => Yii::t('hipanel:certificate', 'GeoTrust SSL Certificates'),
+                'img' => 'https://cdn.ukrnames.com/theme/images/ssl-vendors/geotrust_vendor.png',
+            ],
+            'rapidssl' => [
+                'label' => Yii::t('hipanel:certificate', 'RapidSSL Certificates'),
+                'img' => 'https://cdn.ukrnames.com/theme/images/ssl-vendors/rapidssl_vendor.png',
+            ],
+            'comodo' => [
+                'label' => Yii::t('hipanel:certificate', 'Comodo SSL Certificates'),
+                'img' => 'https://cdn.ukrnames.com/theme/images/ssl-vendors/comodo_vendor.png',
+            ]
+        ];
+    }
+
+    public function getLogo()
+    {
+        $brands = static::brands();
+
+        return $brands[$this->brand]['img'];
+    }
+
+    public function getType()
+    {
+        $data = array_keys(static::types());
+        $v = array_rand(array_flip($data), 2);
+
+        return implode(' ', $v);
+    }
+
+    public function getBrand()
+    {
+        return strtolower(StringHelper::truncateWords($this->name, 1, ''));
     }
 
     public function attributeLabels()
--- src/views/certificate-order/index.php
+++ src/views/certificate-order/index.php
@@ -1,10 +1,86 @@
 <?php
 
+use hipanel\assets\IsotopeAsset;
+use hipanel\modules\certificate\Asset;
+use hipanel\modules\certificate\models\CertificateType;
 use yii\helpers\Html;
+use yii\web\View;
+
+Asset::register($this);
+IsotopeAsset::register($this);
 
 $this->title = Yii::t('hipanel:certificate', 'Get certificate');
 $this->params['breadcrumbs'][] = $this->title;
-$this->registerJs("$('[data-toggle=\"popover\"]').popover();");
+$this->registerJs("
+    $('[data-toggle=\"popover\"]').popover();
+    
+    // init Isotope
+    var grid = $('.certificate-order').isotope({
+        itemSelector: '.info-box',
+        layout: 'vertical',
+        isInitLayout: false
+    });
+    grid.isotope();
+    var filters = {};
+    var filterDisplay = $('#filter-display');
+    
+    $('.filter').on( 'click', 'a, button', function( event ) {
+        var target = $( event.currentTarget );
+        target.toggleClass('active');
+        var isChecked = target.hasClass('active');
+        var group = target.parents('.filter').attr('data-filter-group');
+        var filterGroup = filters[ group ];
+        var filter = target.attr('data-filter');
+        if ( !filterGroup ) {
+            filterGroup = filters[ group ] = [];
+        }
+        
+        // add/remove filter
+        if ( isChecked ) {
+            // add filter
+            filterGroup.push( filter );
+        } else {
+            // remove filter
+            var index = filterGroup.indexOf( filter );
+            filterGroup.splice( index, 1 );
+        }
+        
+        var comboFilter = getComboFilter();
+        grid.isotope({ filter: comboFilter });
+        filterDisplay.text( comboFilter );
+    });
+    
+    function getComboFilter() {
+        var combo = [];
+        for ( var prop in filters ) {
+            var group = filters[ prop ];
+            if ( !group.length ) {
+                // no filters in group, carry on
+                continue;
+            }
+            // add first group
+            if ( !combo.length ) {
+                combo = group.slice(0);
+                continue;
+            }
+            // add additional groups
+            var nextCombo = [];
+            // split group into combo: [ A, B ] & [ 1, 2 ] => [ A1, A2, B1, B2 ]
+            for ( var i=0; i < combo.length; i++ ) {
+                for ( var j=0; j < group.length; j++ ) {
+                    var item = combo[i] + group[j];
+                    nextCombo.push( item );
+                }
+            }
+            combo = nextCombo;
+        }
+        var comboFilter = combo.join(', ');
+        return comboFilter;
+    }
+    
+    
+    
+", View::POS_END);
 $this->registerCss(".popover {width: 300px;}");
 
 ?>
@@ -15,62 +91,66 @@ $this->registerCss(".popover {width: 300px;}");
                 <h3 class="box-title"><?= Yii::t('hipanel:certificate', 'SSL Products') ?></h3>
             </div>
             <div class="box-body no-padding">
-                <ul class="nav nav-pills nav-stacked" data-filter-group="status">
-                    <li class="active">
-                        <a href="#" data-filter=""><?= Yii::t('hipanel:certificate', 'All') ?></a>
-                    </li>
-                    <?php foreach (\hipanel\modules\certificate\models\CertificateType::types() as $key => $filter) : ?>
-                        <li>
-                            <a href="#" class="">
-                                <b><?= $filter['label'] ?></b>
-                                <span class="label label-info">
-                                    <i class="fa fa-info"></i>
-                                </span>
-                            </a>
-                        </li>
+                <div class="btn-group type-buttons filter-type filter" data-filter-group="type">
+                    <?php foreach (CertificateType::types() as $key => $filter) : ?>
+                        <button data-filter=".<?= $key ?>">
+                            <b><?= $filter['label'] ?></b>
+                            <span><?= $filter['text'] ?></span>
+                        </button>
                     <?php endforeach; ?>
-                </ul>
+                </div>
             </div>
         </div>
         <div class="box box-solid">
             <div class="box-header with-border">
-                <h3 class="box-title"><?= Yii::t('hipanel:certificate', 'SSL Products') ?></h3>
+                <h3 class="box-title"><?= Yii::t('hipanel:certificate', 'SSL Brands') ?></h3>
             </div>
             <div class="box-body no-padding">
-                <ul class="nav nav-pills nav-stacked" data-filter-group="status">
-                    <li class="active">
-                        <a href="#" data-filter=""><?= Yii::t('hipanel:certificate', 'All') ?></a>
-                    </li>
-                    <?php foreach (\hipanel\modules\certificate\models\CertificateType::types() as $key => $filter) : ?>
-                        <li>
-                            <a href="#" class="">
-                                <b><?= $filter['label'] ?></b>
-                                <span class="label label-info">
-                                    <i class="fa fa-info"></i>
-                                </span>
-                            </a>
-                        </li>
+                <div class="btn-group type-buttons filter-brand filter" data-filter-group="brand">
+                    <?php foreach (CertificateType::brands() as $key => $brand) : ?>
+                        <button data-filter=".<?= $key ?>">
+                            <b><?= $brand['label'] ?></b>
+                        </button>
                     <?php endforeach; ?>
-                </ul>
+                </div>
             </div>
         </div>
     </div>
     <div class="col-md-9">
-        <?php foreach ($models as $model) : ?>
-            <div class="info-box">
-                <span class="info-box-icon">
-                    <img src="https://cdn.ukrnames.com/theme/images/ssl-vendors/comodo_vendor.png" alt="">
-                </span>
-                <div class="info-box-content">
-                    <span class="info-box-text"><?= $model->name ?></span>
-                    <span class="info-box-number">5,200</span>
-                    <span class="progress-description">
-                        50% Increase in 30 Days
-                    </span>
-                </div>
-                <!-- /.info-box-content -->
+        <div id="filter-display" style="display: none"></div>
+        <div class="info-box hidden-xs">
+            <div class="info-box-content header">
+                <div class="sq"><?= Yii::t('hipanel:certificate', 'Название сертификата') ?></div>
+                <div class="sq text-center">Подходит для</div>
+                <div class="sq text-center">Гарантия&nbsp;&nbsp;<span class="label label-info"><i class="fa fa-info"></i></span></div>
+                <div class="sq text-center">Стоимость сертификата</div>
             </div>
-        <?php endforeach; ?>
+        </div>
+        <div class="certificate-order">
+            <?php foreach ($models as $model) : ?>
+                <?php $type = $model->type; ?>
+                <div class="info-box <?= $model->brand ?> <?= $type ?>">
+                    <span class="info-box-icon">
+                        <?= Html::img($model->logo) ?>
+                    </span>
+                    <div class="info-box-content">
+                        <div class="sq"><a href="#"><b><?= $model->name ?></b></a></div>
+                        <div class="sq hidden-xs text-center">
+                            <ul class="list-unstyled">
+                                <li>Крупный интернет-магазин</li>
+                                <li>Финансовая организация</li>
+                            </ul>
+                        </div>
+                        <div class="sq hidden-xs text-center">$1,750,000</div>
+                        <div class="sq text-center">
+                            <span class="price">46 400 грн. / год</span>
+                            &nbsp;
+                            <a href="#" class="btn btn-success">Заказать</a>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
     </div>
 </div>
 
