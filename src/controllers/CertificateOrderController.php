8474364aea8a8d6ee1958c28ecd92e73e4d7a9e4
tofid <andreyklochok@gmail.com>
2017-02-27 19:54:18 +0200

    minor

--- src/controllers/CertificateOrderController.php
+++ src/controllers/CertificateOrderController.php
@@ -2,26 +2,14 @@
 
 namespace hipanel\modules\certificate\controllers;
 
-use hipanel\modules\certificate\repositories\CertificateTypesRepository;
-use yii\data\ArrayDataProvider;
+use hipanel\modules\certificate\repositories\CertRepository;
 use yii\web\Controller;
 
 class CertificateOrderController extends Controller
 {
-    /**
-     * @var CertificateTypesRepository
-     */
-    private $certificateTypesRepository;
-
-    public function __construct($id, $module, CertificateTypesRepository $certificateTypesRepository, $config = [])
-    {
-        parent::__construct($id, $module, $config = []);
-        $this->certificateTypesRepository = $certificateTypesRepository;
-    }
-
     public function actionIndex()
     {
-        $models = $this->certificateTypesRepository->getTypes();
+        $models = CertRepository::create()->getTypes();
 
         return $this->render('index', [
             'models' => $models,
