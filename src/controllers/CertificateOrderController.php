9ab0a5b4ce43ed644537381f9f73d58b378bd6d7
tofid <andreyklochok@gmail.com>
2017-02-28 20:03:25 +0200

    Added get links modal action

--- src/controllers/CertificateOrderController.php
+++ src/controllers/CertificateOrderController.php
@@ -5,6 +5,7 @@ namespace hipanel\modules\certificate\controllers;
 use hipanel\models\Ref;
 use hipanel\modules\certificate\forms\CsrGeneratorForm;
 use hipanel\modules\certificate\repositories\CertRepository;
+use hipanel\modules\certificate\widgets\PreOrderQuestion;
 use yii\web\Controller;
 
 class CertificateOrderController extends Controller
@@ -16,11 +17,21 @@ class CertificateOrderController extends Controller
         ]);
     }
 
-    public function actionCsrGenerator()
+    public function actionCsrGenerator($productId = null)
     {
+        $model = new CsrGeneratorForm();
+        if ($productId) {
+            $model->productId = $productId;
+        }
+
         return $this->render('csr-generator', [
             'countries' => array_change_key_case(Ref::getList('country_code'), CASE_UPPER),
-            'model' => new CsrGeneratorForm()
+            'model' => $model,
         ]);
+    }
+
+    public function actionGetLinksModal($productId)
+    {
+        return PreOrderQuestion::links($productId);
     }
 }
