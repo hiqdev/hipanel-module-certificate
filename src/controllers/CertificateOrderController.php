3fba48b273b9991f3f9698da246883595f8de6b9
tofid <andreyklochok@gmail.com>
2017-02-28 18:41:11 +0200

    Added CsrGenerator action

--- src/controllers/CertificateOrderController.php
+++ src/controllers/CertificateOrderController.php
@@ -2,6 +2,8 @@
 
 namespace hipanel\modules\certificate\controllers;
 
+use hipanel\models\Ref;
+use hipanel\modules\certificate\forms\CsrGeneratorForm;
 use hipanel\modules\certificate\repositories\CertRepository;
 use yii\web\Controller;
 
@@ -9,10 +11,16 @@ class CertificateOrderController extends Controller
 {
     public function actionIndex()
     {
-        $models = CertRepository::create()->getTypes();
-
         return $this->render('index', [
-            'models' => $models,
+            'models' => CertRepository::create()->getTypes(),
+        ]);
+    }
+
+    public function actionCsrGenerator()
+    {
+        return $this->render('csr-generator', [
+            'countries' => array_change_key_case(Ref::getList('country_code'), CASE_UPPER),
+            'model' => new CsrGeneratorForm()
         ]);
     }
 }
