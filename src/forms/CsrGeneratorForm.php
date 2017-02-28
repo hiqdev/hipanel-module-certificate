b8966106de68a98650274b3e9efaca069af5b46a
tofid <andreyklochok@gmail.com>
2017-02-28 18:40:18 +0200

    Added CsrGeneratorForm

---
+++ src/forms/CsrGeneratorForm.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace hipanel\modules\certificate\forms;
+
+use Yii;
+use yii\base\Model;
+
+class CsrGeneratorForm extends Model
+{
+    public $cn;
+    public $o;
+    public $ou;
+    public $l;
+    public $st;
+    public $c;
+    public $email;
+
+    public function rules()
+    {
+        return [
+            [['cn', 'o', 'ou', 'l', 'st', 'c', 'email'], 'required'],
+//            ['email', 'email'],
+        ];
+    }
+
+    public function attributeLabels()
+    {
+        return [
+            'cn' => Yii::t('hipanel:certificate', 'Common Name'),
+            'o' => Yii::t('hipanel:certificate', 'Organization'),
+            'ou' => Yii::t('hipanel:certificate', 'Department'),
+            'l' => Yii::t('hipanel:certificate', 'City'),
+            'st' => Yii::t('hipanel:certificate', 'State'),
+            'c' => Yii::t('hipanel:certificate', 'Country'),
+            'email' => Yii::t('hipanel:certificate', 'Email'),
+        ];
+    }
+}
