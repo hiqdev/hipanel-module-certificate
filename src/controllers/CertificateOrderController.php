7b0d51102eacf0de2c588b73c476305e321ff684
tofid <andreyklochok@gmail.com>
2017-03-14 17:01:40 +0200

    Added new action Order. Minor fixes in CsrGenerate action

--- src/controllers/CertificateOrderController.php
+++ src/controllers/CertificateOrderController.php
@@ -4,8 +4,11 @@ namespace hipanel\modules\certificate\controllers;
 
 use hipanel\models\Ref;
 use hipanel\modules\certificate\forms\CsrGeneratorForm;
+use hipanel\modules\certificate\forms\OrderForm;
 use hipanel\modules\certificate\repositories\CertRepository;
 use hipanel\modules\certificate\widgets\PreOrderQuestion;
+use Yii;
+use yii\helpers\Url;
 use yii\web\Controller;
 
 class CertificateOrderController extends Controller
@@ -20,14 +23,29 @@ class CertificateOrderController extends Controller
     public function actionCsrGenerator($productId = null)
     {
         $model = new CsrGeneratorForm();
-        if ($productId) {
-            $model->productId = $productId;
-        }
+        $model->productId = $productId;
+        $orderUrl = Url::toRoute(['@certificate/order/order', 'productId' => $model->productId]);
 
         return $this->render('csr-generator', [
             'countries' => array_change_key_case(Ref::getList('country_code'), CASE_UPPER),
             'model' => $model,
+            'orderUrl' => $orderUrl,
         ]);
+    }
+
+    public function actionOrder($productId = null, $email = null)
+    {
+        $model = new OrderForm();
+        $model->attributes = [
+            'productId' => $productId,
+            'email' => $email,
+        ];
+        if (Yii::$app->request->isAjax) {
+            sleep(2);
+            return $this->renderAjax('_orderForm', compact('model'));
+        } else {
+            return $this->render('order', compact('model'));
+        }
     }
 
     public function actionGetLinksModal($productId)
