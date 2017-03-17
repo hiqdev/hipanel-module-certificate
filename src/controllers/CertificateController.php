2ab0ddf091f397cef24a5dff7fb5c4be680ab385
tofid <andreyklochok@gmail.com>
2017-03-17 12:13:30 +0200

    Added default actions

--- src/controllers/CertificateController.php
+++ src/controllers/CertificateController.php
@@ -2,8 +2,21 @@
 
 namespace hipanel\modules\certificate\controllers;
 
+use hipanel\actions\IndexAction;
+use hipanel\actions\ViewAction;
 use hipanel\base\CrudController;
 
 class CertificateController extends CrudController
 {
+    public function actions()
+    {
+        return [
+            'index' => [
+                'class' => IndexAction::class,
+            ],
+            'view' => [
+                'class' => ViewAction::class,
+            ],
+        ];
+    }
 }
