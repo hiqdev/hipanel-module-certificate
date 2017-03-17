dd79ee2bccedf542fd47d22a889251f702f811d7
tofid <andreyklochok@gmail.com>
2017-03-17 12:18:47 +0200

    Added default attributes

--- src/models/Certificate.php
+++ src/models/Certificate.php
@@ -3,7 +3,26 @@
 namespace hipanel\modules\certificate\models;
 
 use hipanel\base\Model;
+use hipanel\base\ModelTrait;
 
 class Certificate extends Model
 {
+    use ModelTrait;
+
+    /** {@inheritdoc} */
+    public function rules()
+    {
+        return [
+            [['id', 'remoteid', 'type_id', 'state_id', 'object_id', 'client_id', 'seller_id'], 'integer'],
+            [['type', 'state', 'client', 'seller', 'validity_start', 'validity_end', 'statuses', 'file', 'object'], 'safe'],
+        ];
+    }
+
+    /** {@inheritdoc} */
+    public function attributeLabels()
+    {
+        return $this->mergeAttributeLabels([
+
+        ]);
+    }
 }
