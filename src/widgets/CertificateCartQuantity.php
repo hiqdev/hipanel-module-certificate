9da08ba8698645dcd151b54ee88ecaaaf2eb915a
tofid <andreyklochok@gmail.com>
2017-03-17 14:18:33 +0200

    Added phpdocs

--- src/widgets/CertificateCartQuantity.php
+++ src/widgets/CertificateCartQuantity.php
@@ -2,6 +2,7 @@
 
 namespace hipanel\modules\certificate\widgets;
 
+use hipanel\modules\certificate\cart\CertificateOrderProduct;
 use Yii;
 use yii\base\Widget;
 use yii\helpers\Html;
@@ -9,14 +10,23 @@ use yii\helpers\Html;
 class CertificateCartQuantity extends Widget
 {
     public $quantityOptions = [];
+
+    /**
+     * CertificateType model id
+     * @var integer
+     */
     public $product_id;
+
+    /**
+     * @var CertificateOrderProduct
+     */
     public $model;
 
     public function run()
     {
         $out = Html::dropDownList('quantity', $this->model->getQuantity(), $this->quantityOptions, ['class' => 'form-control quantity-field']);
         $out .= Html::a(
-            '<i class="fa fa-plus fa-fw"></i>&nbsp;&nbsp;' . Yii::t('hipanel:certificate', 'Add the same item'),
+            '<i class="fa fa-plus fa-fw"></i>&nbsp;&nbsp;' . Yii::t('hipanel:certificate', 'Add the same position'),
             ['@certificate/order/add-to-cart-order', 'product_id' => $this->product_id],
             ['class' => 'btn btn-success btn-flat btn-sm md-mt-10']
         );
