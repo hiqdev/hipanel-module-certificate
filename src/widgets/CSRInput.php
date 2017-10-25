7ee8e2bcd242fc6db9458e0a6f7133236a329e83
tofid <andreyklochok@gmail.com>
2017-10-25 16:53:05 +0300

    Moved AjaxModal from _reissueForm to CSRInput

--- src/widgets/CSRInput.php
+++ src/widgets/CSRInput.php
@@ -3,8 +3,11 @@
 namespace hipanel\modules\certificate\widgets;
 
 use Yii;
+use yii\web\View;
 use yii\base\Widget;
 use yii\bootstrap\Html;
+use hipanel\helpers\Url;
+use hipanel\widgets\AjaxModal;
 
 class CSRInput extends Widget
 {
@@ -14,9 +17,41 @@ class CSRInput extends Widget
 
     public $buttonSelector = 'csr-button';
 
-    public $fqdn;
+    public function init()
+    {
+        $this->registerClientCss();
+        $this->view->on(View::EVENT_END_BODY, function ($event) {
+            echo AjaxModal::widget([
+                'id' => 'csr-modal',
+                'size' => AjaxModal::SIZE_LARGE,
+                'header' => Html::tag('h4', Yii::t('hipanel:certificate', 'Generate CSR form'), ['class' => 'modal-title']),
+                'actionUrl' => Url::to([
+                    '@certificate/csr-generate-form',
+                    'client_id' => $this->model->client_id,
+                    'client' => $this->model->client,
+                    'fqdn' => $this->model->name,
+                ]),
+                'scenario' => 'csr-generate',
+                'toggleButton' => false,
+            ]);
+        });
+    }
 
     public function run()
+    {
+        $html = Html::activeTextarea($this->model, $this->attribute, ['class' => 'form-control', 'rows' => 5]);
+        $html .= Html::button('<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;' . Yii::t('hipanel:certificate', 'Generate CSR'), [
+            'class' => 'btn btn-xs btn-default',
+            'data' => [
+                'toggle' => 'modal',
+                'target' => '#csr-modal',
+            ],
+        ]);
+
+        return Html::tag('div', $html, ['class' => 'csr-input-container']);
+    }
+
+    private function registerClientCss()
     {
         $this->view->registerCss("
         .csr-input-container {
@@ -35,16 +70,5 @@ class CSRInput extends Widget
         }
         
         ");
-        $html = Html::activeTextarea($this->model, $this->attribute, ['class' => 'form-control', 'rows' => 5]);
-        $html .= Html::button('<i class="fa fa-cog" aria-hidden="true"></i>&nbsp;'. Yii::t('hipanel:certificate', 'Generate CSR'), [
-            'class' => 'btn btn-xs btn-default',
-            'data' => [
-                'toggle' => 'modal',
-                'target' => '#csr-modal',
-                'fqdn' => $this->fqdn,
-            ],
-        ]);
-
-        return Html::tag('div', $html, ['class' => 'csr-input-container']);
     }
 }
