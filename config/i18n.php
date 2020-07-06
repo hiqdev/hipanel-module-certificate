9a000b5259bc9be06015f62dcc9be6f26d1767ce
strorch <smy980807@ukr.net>
2020-07-06 18:48:22 +0300

    minor

---
+++ config/i18n.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * SSL certificates module for HiPanel
+ *
+ * @link      https://github.com/hiqdev/hipanel-module-certificate
+ * @package   hipanel-module-certificate
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2017-2019, HiQDev (http://hiqdev.com/)
+ */
+
+return [
+    'languages' => ['ru'],
+    'sourcePath' => dirname(__DIR__) . '/src',
+    'messagePath' => dirname(__DIR__) . '/src/messages',
+    'sort' => false,
+    'removeUnused' => false,
+];
